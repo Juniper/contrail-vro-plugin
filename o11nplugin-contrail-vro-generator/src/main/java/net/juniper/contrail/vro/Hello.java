71129dcb52bef202740144da73b6cfd694a0b90b
Mateusz Figurski <mateusz.figurski@codilime.com>
2017-10-10 13:21:07 +0200

    maven config -the main method call and the generator jar creation disabling

---
+++ o11nplugin-contrail-vro-generator/src/main/java/net/juniper/contrail/vro/Hello.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+ */
+
+package net.juniper.contrail.vro;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("\n------------------------------------------------------------------------");
+        System.out.println("\t\tHello World!");
+        System.out.println("------------------------------------------------------------------------\n");
+    }
+}
