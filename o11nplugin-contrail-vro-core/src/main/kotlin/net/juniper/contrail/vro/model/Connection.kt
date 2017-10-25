1cd900399e6137efc3e3a1fe4b4f82214eb00aac
Filip Chmielewski <filip.chmielewski@codilime.com>
2017-10-25 18:37:48 +0200

    CV-43 Expose ApiConnector methods in Connection class

--- o11nplugin-contrail-vro-core/src/main/kotlin/net/juniper/contrail/vro/model/Connection.kt
+++ o11nplugin-contrail-vro-core/src/main/kotlin/net/juniper/contrail/vro/model/Connection.kt
@@ -7,6 +7,10 @@ package net.juniper.contrail.vro.model
 import com.vmware.o11n.sdk.modeldriven.Findable
 import com.vmware.o11n.sdk.modeldriven.Sid
 import net.juniper.contrail.api.ApiConnector
+import net.juniper.contrail.api.ApiObjectBase
+import net.juniper.contrail.api.ApiPropertyBase
+import net.juniper.contrail.api.ObjectReference
+import java.io.IOException
 
 class Connection(val info: ConnectionInfo, val connector: ApiConnector) : Findable {
     override fun getInternalId(): Sid =
@@ -23,6 +27,74 @@ class Connection(val info: ConnectionInfo, val connector: ApiConnector) : Findab
 
     val port: Int get() =
         info.port
+
+    @Throws(IOException::class)
+    fun create(obj: ApiObjectBase): Boolean {
+        return connector.create(obj)
+    }
+
+    @Throws(IOException::class)
+    fun read(obj: ApiObjectBase): Boolean {
+        return connector.read(obj)
+    }
+
+    @Throws(IOException::class)
+    fun update(obj: ApiObjectBase): Boolean {
+        return connector.update(obj)
+    }
+
+    @Throws(IOException::class)
+    fun delete(obj: ApiObjectBase) {
+        connector.delete(obj)
+    }
+
+    @Throws(IOException::class)
+    fun delete(clazz: Class<out ApiObjectBase>, objectId: String) {
+        connector.delete(clazz, objectId)
+    }
+
+    @Throws(IOException::class)
+    fun findByName(clazz: Class<out ApiObjectBase>, parent: ApiObjectBase, name: String): String? {
+        return connector.findByName(clazz, parent, name)
+    }
+
+    @Throws(IOException::class)
+    fun findByName(clazz: Class<out ApiObjectBase>, ancestorNames: List<String>): String? {
+        return connector.findByName(clazz, ancestorNames)
+    }
+
+    @Throws(IOException::class)
+    fun find(clazz: Class<out ApiObjectBase>, parent: ApiObjectBase, name: String): ApiObjectBase? {
+        return connector.find(clazz, parent, name)
+    }
+
+    @Throws(IOException::class)
+    fun findById(clazz: Class<out ApiObjectBase>, objectId: String): ApiObjectBase? {
+        return connector.findById(clazz, objectId)
+    }
+
+    @Throws(IOException::class)
+    fun findByFQN(clazz: Class<out ApiObjectBase>, fqn: String): ApiObjectBase? {
+        return connector.findByFQN(clazz, fqn)
+    }
+
+    @Throws(IOException::class)
+    fun list(clazz: Class<out ApiObjectBase>): List<ApiObjectBase>? {
+        return connector.list(clazz, null)
+    }
+
+    @Throws(IOException::class)
+    fun <A : ApiPropertyBase> getObjects(
+        clazz: Class<out ApiObjectBase>,
+        references: List<ObjectReference<A>>
+    ): List<ApiObjectBase>? {
+        return connector.getObjects(clazz, references)
+    }
+
+    @Throws(IOException::class)
+    fun sync(uri: String): Boolean {
+        return connector.sync(uri)
+    }
 }
 
 data class ConnectionInfo @JvmOverloads constructor(
