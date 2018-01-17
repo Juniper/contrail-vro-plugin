438a70b03f483130659541609e61a2b421cd131c
Daniel Jasiński <daniel.jasinski@codilime.com>
2018-01-17 13:03:14 +0100

    Do not show in inventory objects without default parent type.

--- plugin/o11nplugin-contrail-config/src/main/kotlin/net/juniper/contrail/vro/config/Config.kt
+++ plugin/o11nplugin-contrail-config/src/main/kotlin/net/juniper/contrail/vro/config/Config.kt
@@ -40,11 +40,15 @@ val Class<*>.isInventoryProperty get() =
 
 val ObjectClass.isRootClass: Boolean get() {
     val parentType = newInstance().defaultParentType
-    if (parentType == null || parentType == "config-root") return true
-    if (simpleName == "Port") return true
+
+    if (parentType == null) return false
+    if (parentType == "config-root") return true
 
     return ! parentType.typeToClassName.isModelClassName
 }
+
+val ObjectClass.isRelateable: Boolean get() =
+    newInstance().defaultParent != null
 
 val inventoryPropertyFilter: PropertyClassFilter = { it.isInventoryProperty }
 val modelClassFilter: ObjectClassFilter = { it.isModelClass }
