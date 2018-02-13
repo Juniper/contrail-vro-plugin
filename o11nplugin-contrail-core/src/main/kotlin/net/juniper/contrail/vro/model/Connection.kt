410cd8d6439d6e4aad2c7e2c1d22f5cec2d2977f
Daniel Jasiński <daniel.jasinski@codilime.com>
2018-02-13 12:24:35 +0100

    Add caching to improve responsiveness of "Edit..." workflows

--- o11nplugin-contrail-core/src/main/kotlin/net/juniper/contrail/vro/model/Connection.kt
+++ o11nplugin-contrail-core/src/main/kotlin/net/juniper/contrail/vro/model/Connection.kt
@@ -4,6 +4,8 @@
 
 package net.juniper.contrail.vro.model
 
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.Cache
 import com.vmware.o11n.sdk.modeldriven.Sid
 import net.juniper.contrail.api.ApiConnector
 import net.juniper.contrail.api.ApiObjectBase
@@ -12,6 +14,23 @@ import net.juniper.contrail.api.ObjectReference
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+private typealias Key = Pair<Class<out ApiObjectBase>, String>
+private typealias Value = ApiObjectBase
+private typealias ObjectCache = Cache<Key, Value>
+
+private val ApiObjectBase.key: Key? get() =
+    uuid?.let { Key(javaClass, it) }
+
+private fun ObjectCache.add(obj: ApiObjectBase) =
+    obj.key?.let { put(it, obj) }
+
+private fun ObjectCache.remove(obj: ApiObjectBase) =
+    obj.key?.let { invalidate(it) }
+
+private fun ObjectCache.remove(clazz: Class<out ApiObjectBase>, id: String?) =
+    id?.let { invalidate(Key(clazz, it)) }
 
 @Suppress("UNCHECKED_CAST")
 class Connection(val info: ConnectionInfo, val connector: ApiConnector) {
@@ -29,17 +48,34 @@ class Connection(val info: ConnectionInfo, val connector: ApiConnector) {
     val displayName: String get() =
         "$name (${info.username ?: "anonymous"}@$host:$port)"
 
+    private val cache: ObjectCache = CacheBuilder.newBuilder()
+        .maximumSize(100)
+        .expireAfterWrite(2, TimeUnit.SECONDS)
+        .build()
+
     @Throws(IOException::class, ConnectionException::class)
     fun create(obj: ApiObjectBase) =
-        asserted("Create") { connector.create(obj) }
+        asserted("Create") {
+            connector.create(obj)
+        }.apply {
+            cache.add(obj)
+        }
 
     @Throws(IOException::class, ConnectionException::class)
     fun read(obj: ApiObjectBase) =
-        asserted("Read") { connector.read(obj) }
+        asserted("Read") {
+            connector.read(obj)
+        }.apply {
+            cache.add(obj)
+        }
 
     @Throws(IOException::class, ConnectionException::class)
     fun update(obj: ApiObjectBase) =
-        asserted("Update") { connector.update(obj) }
+        asserted("Update") {
+            connector.update(obj)
+        }.apply {
+            cache.add(obj)
+        }
 
     @Throws(IOException::class, ConnectionException::class)
     fun sync(uri: String) =
@@ -47,11 +83,12 @@ class Connection(val info: ConnectionInfo, val connector: ApiConnector) {
 
     @Throws(IOException::class)
     fun delete(obj: ApiObjectBase) =
-        connector.delete(obj)
+        // there is no way to check if delete was successful
+        connector.delete(obj).apply { cache.remove(obj) }
 
     @Throws(IOException::class)
     fun delete(clazz: Class<out ApiObjectBase>, objectId: String) =
-        connector.delete(clazz, objectId)
+        connector.delete(clazz, objectId).apply { cache.remove(clazz, objectId) }
 
     fun findByName(clazz: Class<out ApiObjectBase>, parent: ApiObjectBase, name: String): String? =
         safe { connector.findByName(clazz, parent, name) }
@@ -60,10 +97,11 @@ class Connection(val info: ConnectionInfo, val connector: ApiConnector) {
         safe { connector.findByName(clazz, ancestorNames) }
 
     fun <T : ApiObjectBase> find(clazz: Class<T>, parent: ApiObjectBase, name: String): T? =
-        safe { connector.find(clazz, parent, name) as T? }
+        safe { connector.find(clazz, parent, name) as T? }?.also { cache.add(it) }
 
     fun <T : ApiObjectBase> findById(clazz: Class<T>, objectId: String): T? =
-        safe { connector.findById(clazz, objectId) as T? }
+        cache.getIfPresent(Key(clazz, objectId)) as T? ?:
+        safe { connector.findById(clazz, objectId) as T? }?.also { cache.add(it) }
 
     inline fun <reified T : ApiObjectBase> findById(objectId: String): T? =
         findById(T::class.java, objectId)
@@ -72,7 +110,7 @@ class Connection(val info: ConnectionInfo, val connector: ApiConnector) {
         findById(T::class.java, id.getString(T::class.java.simpleName))
 
     fun <T : ApiObjectBase> findByFQN(clazz: Class<T>, fqn: String): T? =
-        safe { connector.findByFQN(clazz, fqn) as T? }
+        safe { connector.findByFQN(clazz, fqn) as T? }?.also { cache.add(it) }
 
     fun <T : ApiObjectBase> list(clazz: Class<T>): List<T>? =
         safe { connector.list(clazz, null) as List<T>? }
