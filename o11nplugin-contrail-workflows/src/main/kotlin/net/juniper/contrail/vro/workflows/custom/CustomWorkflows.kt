84035f97262a7c6ff62d7c50fe22b90c9c006c8d
Filip Chmielewski <filip.chmielewski@codilime.com>
2018-02-16 10:23:04 +0100

    Hide 'edit rule' workflows

--- o11nplugin-contrail-workflows/src/main/kotlin/net/juniper/contrail/vro/workflows/custom/CustomWorkflows.kt
+++ o11nplugin-contrail-workflows/src/main/kotlin/net/juniper/contrail/vro/workflows/custom/CustomWorkflows.kt
@@ -7,15 +7,16 @@ package net.juniper.contrail.vro.workflows.custom
 import net.juniper.contrail.vro.workflows.dsl.WorkflowDefinition
 import net.juniper.contrail.vro.workflows.schema.Schema
 
+// Rule edition workflows are hidden until they are needed
 fun loadCustomWorkflows(schema: Schema): List<WorkflowDefinition> = mutableListOf<WorkflowDefinition>().apply {
     this += createConnectionWorkflow()
     this += deleteConnectionWorkflow()
     this += addRuleToPolicyWorkflow(schema)
-    this += editPolicyRuleWorkflow(schema)
+//    this += editPolicyRuleWorkflow(schema)
     this += removePolicyRuleWorkflow(schema)
     this += createSubnetWorkflow(schema)
     this += deleteSubnetWorkflow()
     this += addRuleToSecurityGroupWorkflow(schema)
-    this += editSecurityGroupRuleWorkflow(schema)
+//    this += editSecurityGroupRuleWorkflow(schema)
     this += removeSecurityGroupRuleWorkflow(schema)
 }
