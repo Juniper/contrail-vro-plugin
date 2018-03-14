20094b5ef17cc20fee8e6e72ccd372c420f1122d
Filip Chmielewski <filip.chmielewski@codilime.com>
2018-03-14 18:59:59 +0100

    Fix SID assignment in action listing ports for network

--- o11nplugin-contrail-gen/src/main/kotlin/net/juniper/contrail/vro/gen/WrapperUtil.kt
+++ o11nplugin-contrail-gen/src/main/kotlin/net/juniper/contrail/vro/gen/WrapperUtil.kt
@@ -13,6 +13,8 @@ import net.juniper.contrail.api.ApiObjectBase
 import net.juniper.contrail.api.ApiPropertyBase
 import net.juniper.contrail.api.ObjectReference
 import net.juniper.contrail.vro.model.Connection
+import net.juniper.contrail.vro.config.pluginName
+import net.juniper.contrail.vro.config.constants.Connection as ConnectionName
 
 class WrapperUtil(val ctx: WrapperContext, val factory: IPluginFactory) {
 
@@ -22,7 +24,7 @@ class WrapperUtil(val ctx: WrapperContext, val factory: IPluginFactory) {
     fun <T : ApiObjectBase, U : ApiPropertyBase, M: Findable>
         references(sid: Sid?, clazz: Class<T>, references: List<ObjectReference<U>>?): List<M> {
         if (sid == null || references == null) return defaultList()
-        val connectionWrapper = factory.find("Connection", sid.toString()) as ModelWrapper? ?: return defaultList()
+        val connectionWrapper = factory.find(ConnectionName, sid.toString()) as ModelWrapper? ?: return defaultList()
         val connection: Connection = connectionWrapper.__getTarget() as Connection
         val objects: List<T> = connection.getObjects(clazz, references) ?: return defaultList()
         return objects.map { it.toWrapper<T, M>(clazz, sid) }
@@ -30,7 +32,7 @@ class WrapperUtil(val ctx: WrapperContext, val factory: IPluginFactory) {
 
     private fun <T : ApiObjectBase, M : Findable> T.toWrapper(clazz: Class<T>, sid: Sid): M {
         val wrapper: M = ctx.createPluginObject(this, clazz)
-        wrapper.internalId = sid.with(clazz.simpleName, uuid)
+        wrapper.internalId = sid.with(clazz.pluginName, uuid)
         return wrapper
     }
 }
