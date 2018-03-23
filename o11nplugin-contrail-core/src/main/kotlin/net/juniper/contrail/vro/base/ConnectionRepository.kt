2b1816ddb7649352a73170ada54cac36728fd4b7
Filip Chmielewski <filip.chmielewski@codilime.com>
2018-03-23 17:05:36 +0100

    [WIP] Add workflow script tests
    Change-Id: I9e64273f24691db9ccb4d797460ca50ae2c8754b

--- o11nplugin-contrail-core/src/main/kotlin/net/juniper/contrail/vro/base/ConnectionRepository.kt
+++ o11nplugin-contrail-core/src/main/kotlin/net/juniper/contrail/vro/base/ConnectionRepository.kt
@@ -80,3 +80,21 @@ class DefaultConnectionRepository
             .toList()
     }
 }
+
+class OneConnectionRepository(private val connection: Connection) : ConnectionRepository {
+    override fun addConnection(item: Connection) {
+    }
+
+    override fun removeConnection(item: Connection) {
+    }
+
+    override fun getConnection(id: Sid): Connection? {
+        return connection
+    }
+
+    override fun findConnections(query: String): List<Connection> {
+        return listOf(connection)
+    }
+
+    override val connections: List<Connection> = listOf(connection)
+}
