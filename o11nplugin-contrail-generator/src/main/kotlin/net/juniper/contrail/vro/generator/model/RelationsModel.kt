9823a13eacb3032b0c91c4a384ebd8070a761ec0
Daniel Jasiński <daniel.jasinski@codilime.com>
2018-03-28 17:04:29 +0200

    Add Contrail Security objects to vRO model.
    Change-Id: I18cfd4b0117cde62f855e48f475c2bc70ab57cac
    Depends-On: I1f5fc44025521ea22592486aeae10471278acfa9
    Depends-On: Id6de5ff5a5e574308f92c3eeed451860768bc94f
    Partial-Bug: #1772594

--- o11nplugin-contrail-generator/src/main/kotlin/net/juniper/contrail/vro/generator/model/RelationsModel.kt
+++ o11nplugin-contrail-generator/src/main/kotlin/net/juniper/contrail/vro/generator/model/RelationsModel.kt
@@ -6,7 +6,6 @@ package net.juniper.contrail.vro.generator.model
 
 import net.juniper.contrail.vro.config.ObjectClass
 import net.juniper.contrail.vro.config.isDirectChild
-import net.juniper.contrail.vro.config.pluginName
 import net.juniper.contrail.vro.config.pluralize
 import net.juniper.contrail.vro.config.toPluginName
 
@@ -20,7 +19,6 @@ data class RelationsModel(
 data class RelationModel(
     val parentName: String,
     val childName: String,
-    val name: String,
     val isDirectChild: Boolean,
     val folderName: String
 ) {
@@ -30,12 +28,12 @@ data class RelationModel(
     val childPluginNameDecapitalized = childPluginName.decapitalize()
     val childPluginNamePluralized = childPluginName.pluralize()
     val childPluginNamePluralizedDecapitalized = childPluginNamePluralized.decapitalize()
+    val getter: String = childName.decapitalize() + "s"
 }
 
 fun Relation.toRelationModel() = RelationModel(
     parentName,
     childName,
-    name,
     childName.isDirectChild,
     folderName
 )
