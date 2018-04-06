b0fcde4d62a3328a88953036e48a1f34e957b26c
Daniel Jasiński <daniel.jasinski@codilime.com>
2018-04-06 15:09:16 +0200

    Include non-updateable properties in auto-generated "Create..." workflows.
    Change-Id: I8a0f062b0230739e1d83f93a00bcf5728043d73e
    Partial-Bug: #1760775

---
+++ o11nplugin-contrail-workflow-dsl/src/main/kotlin/net/juniper/contrail/vro/workflows/schema/Crud.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+ */
+
+package net.juniper.contrail.vro.workflows.schema
+
+val defaultCrud = CRUD("CRUD")
+
+class CRUD(val crud: String) {
+    val isCreate = crud.contains('C')
+    val isRead = crud.contains('R')
+    val isUpdate = crud.contains('U')
+    val isDelete = crud.contains('D')
+
+    val isCreateOnly = isCreate && !isUpdate
+}
+
+fun String?.toCrud() =
+    if (this == null) defaultCrud else CRUD(this)
