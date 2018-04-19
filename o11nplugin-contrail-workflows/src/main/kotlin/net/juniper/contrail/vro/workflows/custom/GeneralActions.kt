2ed5563c03819d02adc0e9110ff21c51cf3445d6
Filip Chmielewski <filip.chmielewski@codilime.com>
2018-04-19 12:48:14 +0200

    Reintroduce workflows for editing rules of Security Group and Network Policy.
    This reverts commit a1095096a1cc594cb51f7534b0a940b35e55a0d4.
    
    Change-Id: I67c56a129c119268fe0f7d938335a06e27fde7f6
    Closes-Bug: #1770174

--- o11nplugin-contrail-workflows/src/main/kotlin/net/juniper/contrail/vro/workflows/custom/GeneralActions.kt
+++ o11nplugin-contrail-workflows/src/main/kotlin/net/juniper/contrail/vro/workflows/custom/GeneralActions.kt
@@ -6,11 +6,12 @@ package net.juniper.contrail.vro.workflows.custom
 
 import net.juniper.contrail.vro.config.listPropertyValue
 import net.juniper.contrail.vro.config.propertyNotNull
+import net.juniper.contrail.vro.config.propertyOfObjectRule
 import net.juniper.contrail.vro.config.propertyValue
 import net.juniper.contrail.vro.config.readSubnet
+import net.juniper.contrail.vro.workflows.dsl.ofType
 import net.juniper.contrail.vro.workflows.model.any
 import net.juniper.contrail.vro.workflows.model.boolean
-import net.juniper.contrail.vro.workflows.dsl.ofType
 import net.juniper.contrail.vro.workflows.model.string
 
 private val item = "item"
@@ -62,3 +63,12 @@ val readSubnetAction = ActionDefinition (
     )
 )
 
+val propertyOfObjectRule = ActionDefinition(
+    name = propertyOfObjectRule,
+    resultType = any,
+    parameters = listOf(
+        "parent" ofType any,
+        "rule" ofType string,
+        "propertyName" ofType string
+    )
+)
