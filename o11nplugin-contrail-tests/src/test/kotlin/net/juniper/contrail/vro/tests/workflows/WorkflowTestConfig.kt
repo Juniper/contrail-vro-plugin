03a589df6e9ec25e948d39c352c341861d31531b
Filip Chmielewski <filip.chmielewski@codilime.com>
2018-05-16 13:36:06 +0200

    Add tools for creating workflow script tests
    Change-Id: I03fc06413c9c0090510d1c13e5ebeca41f739879

---
+++ o11nplugin-contrail-tests/src/test/kotlin/net/juniper/contrail/vro/tests/workflows/WorkflowTestConfig.kt
@@ -0,0 +1,35 @@
+package net.juniper.contrail.vro.tests.workflows
+
+import com.vmware.o11n.plugin.sdk.spring.platform.GlobalPluginNotificationHandler
+import net.juniper.contrail.vro.base.RepositoryInitializer
+import net.juniper.contrail.vro.model.Connection
+import net.juniper.contrail.vro.model.ConnectionInfo
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@Configuration
+@ComponentScan("net.juniper.contrail.vro.base")
+class WorkflowTestConfig {
+
+    @Bean
+    @Primary
+    fun globalPluginNotificationHandler() : GlobalPluginNotificationHandler =
+        globalPluginNotificationHandlerMock
+
+    @Bean
+    @Primary
+    fun repositoryInitializer() : RepositoryInitializer =
+        repositoryInitializerMock
+
+    @Bean
+    fun connection() : Connection {
+        val info = ConnectionInfo("connection name", "host", 8080, "user", "secret")
+        return Connection(info, apiConnectorMock)
+    }
+
+    @Bean
+    @Primary
+    fun connectionRepository() = OneConnectionRepository(connection())
+}
