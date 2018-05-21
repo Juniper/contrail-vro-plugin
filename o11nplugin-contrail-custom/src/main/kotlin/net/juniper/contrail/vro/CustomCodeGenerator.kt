0e2b8de106f0afbd917c8214b694e9dbb1bdd280
Daniel Jasiński <daniel.jasinski@codilime.com>
2018-05-21 12:57:22 +0200

    Configure incremental compilation and JVM options to improve build time.
    Change-Id: Iebd9df6ca26736b383b2c38f50caab530144ec78
    Closes-Bug: #1770567
    (cherry picked from commit 418d8628d3df45afdcba6d048171872a75695afd)

---
+++ o11nplugin-contrail-custom/src/main/kotlin/net/juniper/contrail/vro/CustomCodeGenerator.kt
@@ -0,0 +1,53 @@
+package net.juniper.contrail.vro
+
+import com.vmware.o11n.sdk.modeldrivengen.code.CodeGeneratorConfig
+import com.vmware.o11n.sdk.modeldrivengen.code.DefaultCodeGeneratorConfig
+import com.vmware.o11n.sdk.modeldrivengen.code.DefaultPluginCodeGenerator
+import com.vmware.o11n.sdk.modeldrivengen.code.DefaultRuntimeConfigurationGenerator
+import com.vmware.o11n.sdk.modeldrivengen.code.PluginCodeGenerator
+import com.vmware.o11n.sdk.modeldrivengen.code.RuntimeConfigurationGenerator
+import com.vmware.o11n.sdk.modeldrivengen.mapping.AbstractMapping
+import com.vmware.o11n.sdk.modeldrivengen.model.Plugin
+import net.juniper.contrail.vro.config.packageToPath
+import java.nio.file.Files
+import java.nio.file.Paths
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CustomCodeGenerator @Inject constructor(
+    private val codeDelegate: DefaultPluginCodeGenerator,
+    private val configDelegate: DefaultRuntimeConfigurationGenerator,
+    config: CodeGeneratorConfig,
+    mapping: AbstractMapping
+) : PluginCodeGenerator, RuntimeConfigurationGenerator {
+    private val generate: Boolean = generationRequired(config, mapping)
+
+    init {
+        if (!generate && config is DefaultCodeGeneratorConfig)
+            config.isVerbose = false
+    }
+
+    private fun generationRequired(config: CodeGeneratorConfig, mapping: AbstractMapping): Boolean {
+        val vsoXmlPath = Paths.get(config.javaOutputDir.absolutePath, config.basePackage.packageToPath(), "runtime-config.properties")
+        if (! Files.exists(vsoXmlPath)) return true
+        val mappingPath = Paths.get(mapping.javaClass.protectionDomain.codeSource.location.toURI())
+        val vsoTime = Files.getLastModifiedTime(vsoXmlPath)
+        val mappingTime = Files.getLastModifiedTime(mappingPath)
+        return mappingTime > vsoTime
+    }
+
+    override fun generateJavaCode(mapping: AbstractMapping, plugin: Plugin) {
+        if (generate)
+            codeDelegate.generateJavaCode(mapping, plugin)
+        else
+            println("[INFO] Skipped generation of wrapper classes.")
+    }
+
+    override fun generateRuntimeConfiguration(mapping: AbstractMapping, plugin: Plugin) {
+        if (generate)
+            configDelegate.generateRuntimeConfiguration(mapping, plugin)
+        else
+            println("[INFO] Skipped generation of runtime configuration.")
+    }
+}
--- o11nplugin-contrail-generator/src/main/kotlin/net/juniper/contrail/vro/generator/model/GeneratorEngine.kt
+++ o11nplugin-contrail-generator/src/main/kotlin/net/juniper/contrail/vro/generator/model/GeneratorEngine.kt
@@ -14,9 +14,9 @@ import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 
-data class GeneratorConfig(val outputDir: Path, val verbose: Boolean = true) {
-    constructor(baseDir: Path, packageName: String, verbose: Boolean = true) :
-        this(baseDir.append(packageName.packageToPath()), verbose)
+data class GeneratorConfig(val outputDir: Path, val templateDir: Path, val verbose: Boolean = true, val force: Boolean = false) {
+    constructor(baseDir: Path, templateDir: Path, packageName: String, verbose: Boolean = true) :
+        this(baseDir.append(packageName.packageToPath()), templateDir, verbose)
 }
 
 class GeneratorEngine(private val config: GeneratorConfig, private val freemarker: Configuration) {
@@ -28,9 +28,14 @@ class GeneratorEngine(private val config: GeneratorConfig, private val freemarke
         if (config.verbose)
             println("[INFO] Generating code for $fileName using template $templateName")
         createPackageStructure()
+        val templatePath = config.templateDir.append(templateName)
+        val outputPath = config.outputDir.append(fileName)
+        if (Files.exists(outputPath) && ! config.force && canSkipGeneration(templatePath, outputPath)) {
+            println("[INFO] ---- File up-to-date, skipping generating code.")
+            return
+        }
         val template = freemarker.getTemplate(templateName)
-        val filePath = config.outputDir.append(fileName)
-        FileWriter(filePath.toFile()). use { template.process(model, it) }
+        FileWriter(outputPath.toFile()). use { template.process(model, it) }
     }
 
     @Throws(IOException::class)
@@ -39,6 +44,12 @@ class GeneratorEngine(private val config: GeneratorConfig, private val freemarke
 
     private fun String.toTemplateName(): String =
         decapitalize().replace("\\..+?$".toRegex(), ".ftl")
+
+    private fun canSkipGeneration(templatePath: Path, outputPath: Path): Boolean {
+        val templateTime = Files.getLastModifiedTime(templatePath)
+        val outputTime = Files.getLastModifiedTime(outputPath)
+        return outputTime > templateTime
+    }
 
     private fun createPackageStructure() =
         Files.createDirectories(config.outputDir)
