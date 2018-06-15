f4d097ff2e4a7da1fd89e71ffb85d7aeedd83647
Daniel Jasiński <daniel.jasinski@codilime.com>
2018-06-15 09:22:03 +0200

    Update display name of global Firewall Rules.
    Change-Id: Ib277ec463bacf01ed6adba66066ed61799be05f6
    Closes-Bug: #1770567

--- o11nplugin-contrail-format/src/main/kotlin/net/juniper/contrail/vro/format/DisplayNameFormatter.kt
+++ o11nplugin-contrail-format/src/main/kotlin/net/juniper/contrail/vro/format/DisplayNameFormatter.kt
@@ -10,6 +10,7 @@ import net.juniper.contrail.api.types.FloatingIp
 import net.juniper.contrail.api.types.IpamSubnetType
 import net.juniper.contrail.api.types.QuotaType
 import net.juniper.contrail.api.types.Subnet
+import net.juniper.contrail.vro.format.PropertyFormatter.format
 
 /**
  * Class allows to define custom formatting for selected inventory objects
@@ -20,25 +21,28 @@ object DisplayNameFormatter {
         obj.address
 
     fun format(obj: Subnet): String? =
-        obj.ipPrefix?.let { PropertyFormatter.format(it) }
+        obj.ipPrefix?.let { format(it) }
 
     fun format(obj: ApiObjectBase): String? =
         obj.name
 
     fun format(obj: IpamSubnetType): String? =
-        obj.subnet?.let { PropertyFormatter.format(it) }
+        obj.subnet?.let { format(it) }
 
     fun format(obj: QuotaType): String? =
         "Quotas"
 
     fun format(obj: FirewallRule): String? {
         // `obj.parent?.name` returns null, so we use `obj.qualifiedName.dropLast(1).last()` to get the parent name.
-        val parentName = obj.qualifiedName.dropLast(1).last()
+        val parentName = obj.parentName.let { if (it == "default-policy-management") "global" else it }
         val simpleAction = obj.actionList?.simpleAction
         val direction = obj.direction
-        val service = if (obj.service != null) PropertyFormatter.format(obj.service) else obj.serviceGroup[0].referredName.last()
-        val endpoint1 = PropertyFormatter.format(obj.endpoint1)
-        val endpoint2 = PropertyFormatter.format(obj.endpoint2)
+        val service = if (obj.service != null) format(obj.service) else obj.serviceGroup[0].referredName.last()
+        val endpoint1 = format(obj.endpoint1)
+        val endpoint2 = format(obj.endpoint2)
         return "$parentName: $simpleAction $service  EP1: $endpoint1  $direction  EP2: $endpoint2"
     }
+
+    private val FirewallRule.parentName get() =
+        qualifiedName.dropLast(1).last()
 }
