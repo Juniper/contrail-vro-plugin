e9f753e3d608fff51af7ef477388a17cc10f4164
Filip Chmielewski <filip.chmielewski@codilime.com>
2018-06-19 19:42:20 +0200

    Allow creation of complex workflows
    Change-Id: I1641567bcadaeb748c696760828eaed94e12c4c4
    Closes-Bug: #1770567

---
+++ o11nplugin-contrail-workflow-dsl/src/main/kotlin/net/juniper/contrail/vro/workflows/dsl/Aggregators.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+ */
+
+package net.juniper.contrail.vro.workflows.dsl
+
+class Choice(val name: String, val targetId: Int)
+
+@WorkflowBuilder
+class ChoiceAggregator {
+    private val _choices: MutableList<Choice> = mutableListOf()
+    fun option(name: String, targetId: Int) {
+        _choices.add(Choice(name, targetId))
+    }
+
+    val choices: List<Choice> = _choices
+}
+
+@WorkflowBuilder
+class BindAggregator {
+    private val _inBinds: MutableMap<String, String> = mutableMapOf()
+    private val _outBinds: MutableMap<String, String> = mutableMapOf()
+    fun inputBind(name: String, attributeName: String) {
+        _inBinds[name] = attributeName
+    }
+
+    fun outputBind(name: String, attributeName: String) {
+        _outBinds[name] = attributeName
+    }
+
+    val inBinds: Map<String, String> = _inBinds
+    val outBinds: Map<String, String> = _outBinds
+}
