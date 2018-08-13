cf68eeb26784c7acc09916e9ec3efc901f6aa215
Lukasz Piotrak <lukasz.piotrak@codilime.com>
2018-08-13 17:20:37 +0200

    Change values of read-only properties to null before caching
    Change-Id: Ia51c7d35d70e80e9087c8785b0a9ef2a5343026b
    Closes-bug: #1786972

--- o11nplugin-contrail-generator/src/main/kotlin/net/juniper/contrail/vro/generator/model/Properties.kt
+++ o11nplugin-contrail-generator/src/main/kotlin/net/juniper/contrail/vro/generator/model/Properties.kt
@@ -4,15 +4,24 @@
 
 package net.juniper.contrail.vro.generator.model
 
+import net.juniper.contrail.vro.config.ProjectInfo
+import net.juniper.contrail.vro.config.div
 import net.juniper.contrail.vro.config.isApiTypeClass
 import net.juniper.contrail.vro.config.isGetter
 import net.juniper.contrail.vro.config.kotlinClassName
+import net.juniper.contrail.vro.config.pluginPropertyName
 import net.juniper.contrail.vro.config.propertyName
 import net.juniper.contrail.vro.config.returnListGenericClass
-import net.juniper.contrail.vro.config.pluginPropertyName
 import net.juniper.contrail.vro.config.underscoredNestedName
 import net.juniper.contrail.vro.config.wrapperName
+import net.juniper.contrail.vro.generator.generatedPackageName
+import net.juniper.contrail.vro.generator.generatedSourcesRoot
+import net.juniper.contrail.vro.generator.templatesInClassPath
+import net.juniper.contrail.vro.generator.templatesInResourcesPath
+import net.juniper.contrail.vro.schema.defaultSchema
 import java.lang.reflect.Method
+
+val readOnlyProperties = defaultSchema.propertyComments.filter { it.crud.isReadOnly }
 
 class Property(
     val propertyName: String,
@@ -54,3 +63,26 @@ private fun Method.toPropertyOf(parent: Class<*>): Property? {
 
     return Property(propertyName, pluginPropertyName, type, parent, declaringClass, returnsList)
 }
+
+fun generateSchemaInfo(projectInfo: ProjectInfo) {
+    val templateDir = projectInfo.generatorRoot / templatesInResourcesPath
+    val propertyNameModel = generatePropertyNameModel()
+
+    val coreGeneratorConfig = GeneratorConfig(
+        baseDir = projectInfo.genRoot / generatedSourcesRoot,
+        templateDir = templateDir,
+        packageName = generatedPackageName)
+
+    val coreGenerator = GeneratorEngine(coreGeneratorConfig, templatesInClassPath)
+    coreGenerator.generate(propertyNameModel, "schemaReadOnlyPropertyNames.kt")
+}
+
+fun generatePropertyNameModel() =
+    PropertyNameModel(readOnlyProperties.asReadOnlyPropertyNames)
+
+data class PropertyName(val parentClassName: String, val elementName: String)
+
+data class PropertyNameModel(val propertyNames: List<PropertyName>) : GenericModel()
+
+val Sequence<net.juniper.contrail.vro.schema.Property>.asReadOnlyPropertyNames get() =
+    map { PropertyName(it.parentClassName, it.elementName) }.toList()
