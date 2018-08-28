45cb6563931fff11b554430c8db31d7425a7dec6
dariusz.markiewicz <dariusz.markiewicz@codilime.com>
2018-08-28 15:45:00 +0200

    Add tests for basic workflow generation
    Change-Id: I431919da57db19c3ff535882aa68e86f599f25bf
    Partial-Bug: #1787909

--- o11nplugin-contrail-tests/src/test/groovy/net/juniper/contrail/vro/tests/generator/SimpleWorkflowsGeneratorSpec.groovy
+++ o11nplugin-contrail-tests/src/test/groovy/net/juniper/contrail/vro/tests/generator/SimpleWorkflowsGeneratorSpec.groovy
@@ -4,19 +4,22 @@
 
 package net.juniper.contrail.vro.tests.generator
 
-import net.juniper.contrail.api.types.Project
-import net.juniper.contrail.api.types.VirtualNetwork
+import net.juniper.contrail.vro.tests.TestUtilsKt
 import net.juniper.contrail.vro.workflows.util.DslUtilsKt
 
 class SimpleWorkflowsGeneratorSpec extends GeneratorSpec{
 
-    def "expected simple workflows are generated"() {
+    def "top level models will have create, edit and delete workflows generated"() {
         given:
-        def modelClasses = [VirtualNetwork.class.simpleName].toSet()
+        def modelClasses = [topLevelModel.simpleName].toSet()
 
-        def config = createConfig(modelClasses: modelClasses)
+        def config = createConfig(
+            modelClasses: modelClasses
+        )
 
-        def expectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(VirtualNetwork), DslUtilsKt.deleteWorkflowName(VirtualNetwork)]
+        def expectedGeneratedWorkflows = [DslUtilsKt.deleteWorkflowName(topLevelModel),
+                                          DslUtilsKt.createSimpleWorkflowName(topLevelModel),
+                                          DslUtilsKt.editWorkflowName(topLevelModel)]
 
         when: "generating workflows"
         def generatedWorkflowNames = generateSimpleWorkflows(config)
@@ -25,18 +28,56 @@ class SimpleWorkflowsGeneratorSpec extends GeneratorSpec{
         include(generatedWorkflowNames, expectedGeneratedWorkflows)
     }
 
-    def "declared custom workflows are not generated"() {
+    def "non-top level models will have edit and delete workflows generated"() {
         given:
-        def modelClasses = [VirtualNetwork.class.simpleName].toSet()
-        def customEditWorkflows = [VirtualNetwork.class.simpleName].toSet()
+        def modelClasses = [nonTopLevelModel.simpleName].toSet()
+
+        def config = createConfig(modelClasses: modelClasses)
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(nonTopLevelModel), DslUtilsKt.deleteWorkflowName(nonTopLevelModel)]
+        def notExpectedGeneratedWorkflows = [DslUtilsKt.createSimpleWorkflowName(nonTopLevelModel)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+        and: "generated methods does not include create method"
+        notInclude(generatedWorkflowNames, notExpectedGeneratedWorkflows)
+    }
+
+    def "non-top level models will have create, edit and delete workflows generated if we add its model parent"() {
+        given:
+        def (parent, child) = parentChildPair
+        def modelClasses = [parent.simpleName, child.simpleName].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses
+        )
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(child),
+                                          DslUtilsKt.deleteWorkflowName(child),
+                                          DslUtilsKt.createSimpleWorkflowName(child)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+    }
+
+    def "adding model to customEditWorkflows results in not generating edit workflow"() {
+        given:
+        def modelClasses = [nonTopLevelModel.simpleName].toSet()
+        def customEditWorkflows = [nonTopLevelModel.simpleName].toSet()
 
         def config = createConfig(
             modelClasses: modelClasses,
             customEditWorkflows: customEditWorkflows
         )
 
-        def expectedGeneratedWorkflows = [DslUtilsKt.deleteWorkflowName(VirtualNetwork)]
-        def notExpectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(VirtualNetwork)]
+        def expectedGeneratedWorkflows = [DslUtilsKt.deleteWorkflowName(nonTopLevelModel)]
+        def notExpectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(nonTopLevelModel)]
 
         when: "generating workflows"
         def generatedWorkflowNames = generateSimpleWorkflows(config)
@@ -47,20 +88,239 @@ class SimpleWorkflowsGeneratorSpec extends GeneratorSpec{
         notInclude(generatedWorkflowNames, notExpectedGeneratedWorkflows)
     }
 
-    def "workflow 'create virtual network' is generated when Project is included in modelClasses"() {
+    def "adding model to customDeleteWorkflows results in not generating delete workflow"() {
         given:
-        def modelClasses = [VirtualNetwork.class.simpleName, Project.class.simpleName].toSet()
+        def modelClasses = [nonTopLevelModel.simpleName].toSet()
+        def customDeleteWorkflows = [nonTopLevelModel.simpleName].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses,
+            customDeleteWorkflows: customDeleteWorkflows
+        )
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(nonTopLevelModel)]
+        def notExpectedGeneratedWorkflows = [DslUtilsKt.deleteWorkflowName(nonTopLevelModel)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+        and: "generated methods does not include custom methods"
+        notInclude(generatedWorkflowNames, notExpectedGeneratedWorkflows)
+    }
+
+    def "adding workflow to customCreateWorkflows results in not generating create workflow"() {
+        given:
+        def modelClasses = [topLevelModel.simpleName].toSet()
+        def customCreateWorkflows = [topLevelModel.simpleName].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses,
+            customCreateWorkflows: customCreateWorkflows
+        )
+
+        def notExpectedGeneratedWorkflows = [DslUtilsKt.createSimpleWorkflowName(topLevelModel)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods does not include custom method names"
+        notInclude(generatedWorkflowNames, notExpectedGeneratedWorkflows)
+    }
+
+    def "adding workflow to customCreateWorkflows does not affect generating other workflows"() {
+        given:
+        def (parent, child) = parentChildPair
+        def modelClasses = [child.simpleName, parent.simpleName].toSet()
+        def customCreateWorkflows = [child.simpleName, parent.simpleName].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses,
+            customCreateWorkflows: customCreateWorkflows
+        )
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(child),
+                                          DslUtilsKt.editWorkflowName(parent),
+                                          DslUtilsKt.deleteWorkflowName(child),
+                                          DslUtilsKt.deleteWorkflowName(parent)]
+
+        def notExpectedGeneratedWorkflows = [DslUtilsKt.createSimpleWorkflowName(child),
+                                             DslUtilsKt.createSimpleWorkflowName(parent)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+        and: "generated methods does not include custom methods"
+        notInclude(generatedWorkflowNames, notExpectedGeneratedWorkflows)
+    }
+
+    def "adding models which are in relation results in generating reference workflows"() {
+        given:
+        def (referencer, referenced) = relatedPair
+        def modelClasses = [referencer.simpleName, referenced.simpleName].toSet()
 
         def config = createConfig(
             modelClasses: modelClasses
         )
 
-        def expectedGeneratedWorkflows = [DslUtilsKt.deleteWorkflowName(VirtualNetwork),
-                                          DslUtilsKt.deleteWorkflowName(Project),
-                                          DslUtilsKt.createSimpleWorkflowName(VirtualNetwork),
-                                          DslUtilsKt.createSimpleWorkflowName(Project),
-                                          DslUtilsKt.editWorkflowName(VirtualNetwork),
-                                          DslUtilsKt.editWorkflowName(Project)]
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.addRelationWorkflowName(referencer, referenced),
+                                          DslUtilsKt.removeRelationWorkflowName(referencer, referenced)]
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+    }
+
+    def "adding models which are in relation does not affect generating other workflows"() {
+        given:
+        def (referencer, referenced) = relatedPair
+        println(referencer)
+        def modelClasses = [referencer.simpleName, referenced.simpleName].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses
+        )
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.deleteWorkflowName(referencer),
+                                          DslUtilsKt.editWorkflowName(referencer),
+                                          DslUtilsKt.deleteWorkflowName(referenced)]
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+    }
+
+    def "adding models which are not in relation does not result in generating reference workflows"() {
+        given:
+        def (someModel, anotherModel) = unrelatedPair
+        def modelClasses = [someModel.simpleName, anotherModel.simpleName].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses
+        )
+
+        def notExpectedGeneratedWorkflows = [DslUtilsKt.addRelationWorkflowName(someModel, anotherModel),
+                                             DslUtilsKt.addRelationWorkflowName(anotherModel, someModel),
+                                             DslUtilsKt.removeRelationWorkflowName(someModel, anotherModel),
+                                             DslUtilsKt.removeRelationWorkflowName(anotherModel, someModel)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods does not include all of the reference method names"
+        notInclude(generatedWorkflowNames, notExpectedGeneratedWorkflows)
+    }
+
+    def "adding models which are not in relation does not affect generating other workflows"() {
+        given:
+        def (someModel, anotherModel) = unrelatedPair
+        def modelClasses = [someModel.simpleName, anotherModel.simpleName].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses
+        )
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(someModel),
+                                          DslUtilsKt.editWorkflowName(anotherModel),
+                                          DslUtilsKt.deleteWorkflowName(someModel),
+                                          DslUtilsKt.deleteWorkflowName(anotherModel)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+    }
+
+    def "adding model to customAddReference results in not generating add reference workflow"() {
+        given:
+        def (referencer, referenced) = relatedPair
+        def modelClasses = [referencer.simpleName, referenced.simpleName].toSet()
+        def customAddReference = [TestUtilsKt.getPairOf(referencer, referenced)].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses,
+            customAddReference: customAddReference
+        )
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.removeRelationWorkflowName(referencer, referenced)]
+
+        def notExpectedGeneratedWorkflows = [DslUtilsKt.addRelationWorkflowName(referencer, referenced)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+        and: "generated methods does not include custom methods"
+        notInclude(generatedWorkflowNames, notExpectedGeneratedWorkflows)
+    }
+
+    def "adding model to customAddReference does not affect generating other workflows"() {
+        given:
+        def (referencer, referenced) = relatedPair
+        def modelClasses = [referencer.simpleName, referenced.simpleName].toSet()
+        def customAddReference = [TestUtilsKt.getPairOf(referencer, referenced)].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses,
+            customAddReference: customAddReference
+        )
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(referencer),
+                                          DslUtilsKt.deleteWorkflowName(referencer)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+    }
+
+    def "adding model to customRemoveReference results in not generating remove reference workflow"() {
+        given:
+        def (referencer, referenced) = relatedPair
+        def modelClasses = [referencer.simpleName, referenced.simpleName].toSet()
+        def customRemoveReference = [TestUtilsKt.getPairOf(referencer, referenced)].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses,
+            customRemoveReference: customRemoveReference
+        )
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.addRelationWorkflowName(referencer, referenced)]
+
+        def notExpectedGeneratedWorkflows = [DslUtilsKt.removeRelationWorkflowName(referencer, referenced)]
+
+        when: "generating workflows"
+        def generatedWorkflowNames = generateSimpleWorkflows(config)
+
+        then: "generated methods include all of the expected method names"
+        include(generatedWorkflowNames, expectedGeneratedWorkflows)
+        and: "generated methods does not include custom methods"
+        notInclude(generatedWorkflowNames, notExpectedGeneratedWorkflows)
+    }
+
+    def "adding model to customRemoveReference does not affect generating other workflows"() {
+        given:
+        def (referencer, referenced) = relatedPair
+        def modelClasses = [referencer.simpleName, referenced.simpleName].toSet()
+        def customRemoveReference = [TestUtilsKt.getPairOf(referencer, referenced)].toSet()
+
+        def config = createConfig(
+            modelClasses: modelClasses,
+            customRemoveReference: customRemoveReference
+        )
+
+        def expectedGeneratedWorkflows = [DslUtilsKt.editWorkflowName(referencer),
+                                          DslUtilsKt.deleteWorkflowName(referencer)]
 
         when: "generating workflows"
         def generatedWorkflowNames = generateSimpleWorkflows(config)
