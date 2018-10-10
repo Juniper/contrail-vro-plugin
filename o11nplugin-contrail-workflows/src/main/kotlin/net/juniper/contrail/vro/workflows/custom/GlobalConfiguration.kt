913fa5fb3154fdf1f4ee87456b0e89c5b148396d
Filip Chmielewski <filip.chmielewski@codilime.com>
2018-10-10 20:12:51 +0200

    Add global config edit workflow
    Change-Id: I467af8d28f554bce6f7a13385a6e8df7d114b86f

---
+++ o11nplugin-contrail-workflows/src/main/kotlin/net/juniper/contrail/vro/workflows/custom/GlobalConfiguration.kt
@@ -0,0 +1,53 @@
+package net.juniper.contrail.vro.workflows.custom
+
+import net.juniper.contrail.api.types.GlobalSystemConfig
+import net.juniper.contrail.api.types.GlobalVrouterConfig
+import net.juniper.contrail.vro.config.constants.Connection
+import net.juniper.contrail.vro.config.constants.editGlobalConfiguration
+import net.juniper.contrail.vro.config.parentConnection
+import net.juniper.contrail.vro.schema.Schema
+import net.juniper.contrail.vro.schema.propertyDescription
+import net.juniper.contrail.vro.schema.simpleTypeConstraints
+import net.juniper.contrail.vro.workflows.dsl.WhenNonNull
+import net.juniper.contrail.vro.workflows.dsl.WorkflowDefinition
+import net.juniper.contrail.vro.workflows.dsl.inCategory
+import net.juniper.contrail.vro.workflows.dsl.workflow
+import net.juniper.contrail.vro.workflows.model.array
+import net.juniper.contrail.vro.workflows.model.boolean
+import net.juniper.contrail.vro.workflows.model.reference
+import net.juniper.contrail.vro.workflows.model.string
+
+val securityDraftModeParameterName = "enableSecurityPolicyDraft"
+val encapsulationPrioritiesParameterName = "encapsulationPriorities"
+val forwardingModeParameterName = "forwardingMode"
+val encapsulationPredefinedAnswers = listOf("MPLSoGRE", "MPLSoUDP", "VXLAN")
+
+// TODO: DATA BINDINGS
+internal fun editGlobalConfig(schema: Schema): WorkflowDefinition =
+    workflow(editGlobalConfiguration).withScriptFile("editGlobalConfig") {
+        step("Connection") {
+            parameter(parentConnection, Connection.reference) {
+                description = "Contrail controller whose config will be changed"
+                mandatory = true
+            }
+        }
+        step("System config") {
+            visibility = WhenNonNull(parentConnection)
+            parameter(securityDraftModeParameterName, boolean) {
+                description = schema.propertyDescription<GlobalSystemConfig>(securityDraftModeParameterName)
+                mandatory = true
+            }
+        }
+        step("Virtual Routers config") {
+            visibility = WhenNonNull(parentConnection)
+            parameter(encapsulationPrioritiesParameterName, array(string)) {
+                description = schema.propertyDescription<GlobalVrouterConfig>(encapsulationPrioritiesParameterName)
+                // for some reason, trying to extract these from schema returns an error
+                predefinedAnswers = encapsulationPredefinedAnswers
+            }
+            parameter(forwardingModeParameterName, string) {
+                description = schema.propertyDescription<GlobalVrouterConfig>(forwardingModeParameterName)
+                additionalQualifiers += schema.simpleTypeConstraints<GlobalVrouterConfig>(forwardingModeParameterName)
+            }
+        }
+    }.inCategory("Global Configuration")
